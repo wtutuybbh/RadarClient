fe2d2d9ff676f12e5532635d37e283e1f6aa5fa1
plazmer <psytechno@mail.ru>
2016-11-08 23:23:12 +0300

    GRIDCELL without overhead

--- RadarClient/CRCGridCell.h
+++ RadarClient/CRCGridCell.h
@@ -1,23 +1,7 @@
 ﻿#pragma once
 #include "stdafx.h"
 
-#define CAT( A, B ) A ## B
-#define SELECT( NAME, NUM ) CAT( NAME ## _, NUM )
-
-#define EXPAND(x) x
-
-#define GET_COUNT( _1, _2, _3, _4, _5, _6, COUNT, ... ) COUNT
-#define VA_SIZE( ... ) EXPAND(GET_COUNT( __VA_ARGS__ , 6, 5, 4, 3, 2, 1, 0 ))
-
-#define VA_SELECT( NAME, ... ) SELECT( NAME, VA_SIZE(__VA_ARGS__) )(__VA_ARGS__)
-
-#define GRIDCELL( ... ) VA_SELECT( MY_IMPL, __VA_ARGS__ )
-#define MY_IMPL_1(...) not_implemented_1
-#define MY_IMPL_2(...) *(&(CRCGridCell::CRCGridCell(__VA_ARGS__))) 
-#define MY_IMPL_3(...) not_implemented_3
-#define MY_IMPL_4(...) *(&(CRCGridCell::CRCGridCell(__VA_ARGS__))) 
-
-//#define GRIDCELL(...) *(&(CRCGridCell::CRCGridCell(__VA_ARGS__))) 
+#define GRIDCELL(...) *(&(CRCGridCell::CRCGridCell(__VA_ARGS__))) 
 
 class CRCGridCell
 {	
